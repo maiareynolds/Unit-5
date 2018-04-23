961a2e75a0c9cc72499c6d61316091043362a677
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-04-23 08:42:14 -0400

    Updated from Brython Server: 4/23/2018 8:42:14 AM

--- middleWord.py
+++ middleWord.py
@@ -1,1 +1,10 @@
+#Maia Reynolds
+#4/23/18
+#middleWord.py - prints middle word
 
+words=input("Enter a list of words: ").split(" ")
+if len(words)%2==0:
+    print(words[((len(words)/2)-1)])
+    print(words[((len(words)/2))])
+else:
+    print(words[((len(words)/2)-.5)])
