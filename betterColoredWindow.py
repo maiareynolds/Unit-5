6c3486a10b2e27674ce651af7c247cc035be9c62
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-04-25 08:34:16 -0400

    Updated from Brython Server: 4/25/2018 8:34:15 AM

--- betterColoredWindow.py
+++ betterColoredWindow.py
@@ -1,1 +1,14 @@
+#Maia Reynolds
+#4/25/18
+#betterColoredWindow.py - improving colorChangeWindow program
 
+from ggame import *
+from random import randint
+
+color=[Color(0xFF0000,1),Color(0x00FF00,1),Color(0x0000FF,1)]
+def mouseClick(event):
+    num=randint(1,3)
+    RectangleAsset(1200,800,LineStyle(1,color[num-1]),color[num-1])
+
+App().listenMouseEvent("click",mouseClick)
+App().run()
