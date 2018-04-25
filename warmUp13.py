21f83d9a101c6e5ac256d1a328f311b5e4b6d630
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-04-25 08:21:39 -0400

    Updated from Brython Server: 4/25/2018 8:21:38 AM

--- warmUp13.py
+++ warmUp13.py
@@ -1,1 +1,14 @@
+#Maia Reynolds
+#4/25/18
+#warmUp13.py - list of 20 random numbers, find sum, min, max, etc.
 
+from random import randint
+numbers=[]
+i=0
+while i<20:
+    numbers.append(randint(1,100))
+    i+=1
+print(numbers)
+print("The sum is ",sum(numbers))
+print("The minimum number is",min(numbers))
+print("The maximum number is ",max(numbers))
