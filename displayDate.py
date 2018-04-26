48734144bea7c21b0ecb88c4775f40318b25b337
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-04-26 08:49:33 -0400

    Updated from Brython Server: 4/26/2018 8:49:32 AM

--- displayDate.py
+++ displayDate.py
@@ -2,4 +2,15 @@
 #4/25/18
 #displayDate.py - prints date
 
-from import
+from datetime import date
+
+today=date.today()
+
+weekdaynum=today.weekday()
+weekday=["Monday,","Tuesday,","Wednesday,","Thursday,","Friday,","Saturday,","Sunday,"]
+
+monthnum=today.month
+month=["January","February","March","April"]
+
+
+print("Today is",weekday[weekdaynum],month[monthnum])
