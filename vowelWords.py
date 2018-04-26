36205c45f02f96b481403b3d46fdcddd442aa32a
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-04-26 08:29:36 -0400

    Updated from Brython Server: 4/26/2018 8:29:35 AM

--- vowelWords.py
+++ vowelWords.py
@@ -1,1 +1,9 @@
+#Maia Reynolds
+#4/26/18
+#vowelWords.py - how to treat strings as lists
 
+words=input("Enter a list of words: ").split(" ")
+
+for item in words:
+    if item[0] in "AEIOUaeiou":
+        print(item)
