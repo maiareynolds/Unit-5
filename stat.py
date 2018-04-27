0f5aba436fb5351177a2dfde479c5868d871a08b
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-04-27 08:49:35 -0400

    Updated from Brython Server: 4/27/2018 8:49:33 AM

--- stat.py
+++ stat.py
@@ -1,1 +1,14 @@
+#Maia Reynolds
+#4/27/18
+#stat.py - takes list, prints min, max, median, mode, mean
 
+num=input("Enter a list of numbers: ").split(" ")
+
+maximum=0
+for item in num:
+    if int(item)>=maximum:
+        maximum=int(item)
+print("Max =",maximum)
+minumum=maximum
+for item in num:
+print(minimum)
