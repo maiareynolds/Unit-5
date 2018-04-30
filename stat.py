36aa523b023175a38873672724c81c024ae5e5db
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-04-30 08:48:51 -0400

    Updated from Brython Server: 4/30/2018 8:48:50 AM

--- stat.py
+++ stat.py
@@ -4,13 +4,36 @@
 
 num=input("Enter a list of numbers: ").split(" ")
 
+#max
 maximum=0
 for item in num:
     if int(item)>=maximum:
         maximum=int(item)
 print("Max =",maximum)
+
+#min
 minimum=maximum
 for item in num:
     if int(item)<=minimum:
-        
-print(minimum)
+        minimum=int(item)
+print("Min =",minimum)
+
+#mean
+sum=0
+for item in num:
+    sum+=int(item)
+print("Mean =",sum/len(num))
+
+#median
+num=num.sort()
+if len(num)%2==0:
+    print("Median =",(int(num[((len(num)/2)-1)])+int(num[((len(num)/2))]))/2)
+else:
+    print("Median =",num[((len(num)/2)-.5)])
+
+#mode
+times=0
+for item in num:
+    if num.count(item)>=times:
+        times=int(item)
+print("Mode =",times)
