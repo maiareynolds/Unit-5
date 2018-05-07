c29933e2df96285a2d8b3f19138b0b8d00060f99
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-05-07 06:50:18 -0400

    Updated from Brython Server: 5/7/2018 6:50:17 AM

--- displayDate.py
+++ displayDate.py
@@ -2,16 +2,16 @@
 #4/25/18
 #displayDate.py - prints date
 
-from datetime import date
+from datetime import date #imports date
 
-today=date.today()
+today=date.today() #makes variable today = the date today
 
-weekdaynum=today.weekday()
-day=today.day
-monthnum=today.month
-yearnum=today.year
-weekday=["Monday,","Tuesday,","Wednesday,","Thursday,","Friday,","Saturday,","Sunday,"]
-month=["January","February","March","April","May","June","July","August","September","October","November","December"]
+day=today.day #makes day = the day of the month
+year=today.year #makes year = to the year
+weekday=["Monday,","Tuesday,","Wednesday,","Thursday,","Friday,","Saturday,","Sunday,"]#list of days
+month=["January","February","March","April","May","June","July","August","September","October","November","December"]#list of months
 
-
-print("Today is",weekday[weekdaynum],month[monthnum-1],day,yearnum)
+#prints today's date
+#takes the number of the day of the week it is (today.weekday()) from weekday, the list of days, and prints the word
+#takes the month number (today.month)-1 because lists start at 0, from the list of months, and prints the word
+print("Today is",weekday[today.weekday()],month[today.month-1],day,year)
