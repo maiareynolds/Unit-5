7fdee5172ca9f415eed21e0e8547e92deb3125d1
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-05-07 08:34:38 -0400

    Updated from Brython Server: 5/7/2018 8:34:38 AM

--- quiz5.py
+++ quiz5.py
@@ -1,27 +1,28 @@
 #Maia Reynolds
 #5/7/18
-#quiz5.py - 
+#quiz5.py - four functions with lists
 
-def penultimate(list1):
+def penultimate(list1): #takes second to last item in list
     return list1[-2]
 
-def plusEquals(list2,integer):
+def plusEquals(list2,integer): #makes new list with integer added to each item
     newlist=[]
     for item in list2:
-        newlist.append(item+integer)
+        newlist.append(item+integer)#adds new number to newlist
     return newlist
 
-def smallest(list3):
-    list3.sort()
-    return list3[0]
+def smallest(list3):#returns smallest number
+    list3.sort()#orders list from smallest to largest number
+    return list3[0]#takes first number
 
-def decimalRange(num1,num2,num3):
+def decimalRange(num1,num2,num3):#makes list of range from num1 to num2, using intervals of num3
     list4=[]
-    while num1<=num2:
-        list4.append(num1)
+    while num1<num2:
+        list4.append(num1)#adds new number to list
         num1+=num3
     return list4
 
+#test
 print(penultimate([3,4,5,6,7]))
 print(plusEquals([1,2,3,4],10))
 print(smallest([1,2,3,4]))
